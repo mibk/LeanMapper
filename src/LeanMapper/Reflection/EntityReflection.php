67af868365de185b90566cbb11930354f94f3710
Michal Bohuslávek <mbohuslavek@gmail.com>
2025-04-08 11:02:48 +0200

    Put license to plain text file LICENSE

--- src/LeanMapper/Reflection/EntityReflection.php
+++ src/LeanMapper/Reflection/EntityReflection.php
@@ -6,7 +6,7 @@
  * Copyright (c) 2013 Vojtěch Kohout (aka Tharos)
  *
  * For the full copyright and license information, please view the file
- * license.md that was distributed with this source code.
+ * LICENSE that was distributed with this source code.
  */
 
 namespace LeanMapper\Reflection;
